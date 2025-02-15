1f8cefe25906490af344ab5240e3fe6a8b1e0d1f
Renato <renato.henrique_98@hotmail.com>
2025-02-15 23:54:48 +0000

    feat: add lazydocker

--- server/src/main/java/com/github/granas/HomeController.java
+++ server/src/main/java/com/github/granas/HomeController.java
@@ -16,26 +16,26 @@ public class HomeController {
 
   @Getter
   @AllArgsConstructor
-  static class Movie {
-    private String name;
+  static class Quote {
+    private String movie;
     private String quote;
   }
 
   @GetMapping
   @Operation(hidden = true)
-  public Movie home() {
+  public Quote home() {
     var quotes = List.of(
-      new Movie("Taxi Driver", "You talking to me?"),
-      new Movie("The Godfather", "I'm going to make him an offer he can't refuse."),
-      new Movie("Star Wars", "May the Force be with you."),
-      new Movie("Jaws", "You're gonna need a bigger boat"),
-      new Movie("Cidade de Deus", "Dadinho é o caralho! meu nome é Zé Pequeno, porra!"),
-      new Movie("Scarface", "Say “hello” to my little friend!"),
-      new Movie("Dr. No", "Bond. James Bond."),
-      new Movie("Terminator 2","Hasta la vista, baby."),
-      new Movie("The Sixth Sense", "I see dead people."),
-      new Movie("Apollo 13", "Houston, we have a problem."),
-      new Movie("O Auto da Compadecida", "Só sei que foi assim.")
+      new Quote("Taxi Driver", "You talking to me?"),
+      new Quote("The Godfather", "I'm going to make him an offer he can't refuse."),
+      new Quote("Star Wars", "May the Force be with you."),
+      new Quote("Jaws", "You're gonna need a bigger boat"),
+      new Quote("Cidade de Deus", "Dadinho é o caralho! meu nome é Zé Pequeno, porra!"),
+      new Quote("Scarface", "Say “hello” to my little friend!"),
+      new Quote("Dr. No", "Bond. James Bond."),
+      new Quote("Terminator 2","Hasta la vista, baby."),
+      new Quote("The Sixth Sense", "I see dead people."),
+      new Quote("Apollo 13", "Houston, we have a problem."),
+      new Quote("O Auto da Compadecida", "Só sei que foi assim.")
     );
 
     return quotes.get(new Random().nextInt(quotes.size()));
