9d5548b135e75ee4182d745c6e559a3b40733ac7
Renato <renato.henrique_98@hotmail.com>
2025-02-16 00:48:10 +0000

    feat: improve home

--- server/src/main/java/com/github/granas/HomeController.java
+++ server/src/main/java/com/github/granas/HomeController.java
@@ -1,41 +1,36 @@
 package com.github.granas;
 
-import io.swagger.v3.oas.annotations.Operation;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
+import java.util.List;
+import java.util.Random;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.Random;
+import com.github.granas.shared.http.Quote;
+
+import io.swagger.v3.oas.annotations.Operation;
 
 @RestController
 @RequestMapping("/")
 public class HomeController {
-
-  @Getter
-  @AllArgsConstructor
-  static class Quote {
-    private String movie;
-    private String quote;
-  }
-
   @GetMapping
   @Operation(hidden = true)
   public Quote home() {
     var quotes = List.of(
-      new Quote("Taxi Driver", "You talking to me?"),
-      new Quote("The Godfather", "I'm going to make him an offer he can't refuse."),
-      new Quote("Star Wars", "May the Force be with you."),
-      new Quote("Jaws", "You're gonna need a bigger boat"),
-      new Quote("Cidade de Deus", "Dadinho é o caralho! meu nome é Zé Pequeno, porra!"),
-      new Quote("Scarface", "Say “hello” to my little friend!"),
-      new Quote("Dr. No", "Bond. James Bond."),
-      new Quote("Terminator 2","Hasta la vista, baby."),
-      new Quote("The Sixth Sense", "I see dead people."),
-      new Quote("Apollo 13", "Houston, we have a problem."),
-      new Quote("O Auto da Compadecida", "Só sei que foi assim.")
+      Quote.from("Taxi Driver", "You talking to me?"),
+      Quote.from("The Godfather", "I'm going to make him an offer he can't refuse."),
+      Quote.from("Star Wars", "May the Force be with you."),
+      Quote.from("Jaws", "You're gonna need a bigger boat"),
+      Quote.from("Cidade de Deus", "Dadinho é o caralho! meu nome é Zé Pequeno, porra!"),
+      Quote.from("Scarface", "Say “hello” to my little friend!"),
+      Quote.from("Dr. No", "Bond. James Bond."),
+      Quote.from("Terminator 2","Hasta la vista, baby."),
+      Quote.from("The Sixth Sense", "I see dead people."),
+      Quote.from("Apollo 13", "Houston, we have a problem."),
+      Quote.from("O Auto da Compadecida", "Só sei que foi assim."),
+      Quote.from("Fight Club", "The first rule of Fight Club is. You do not talk about Fight Club."),
+      Quote.from("Toy Story", "You Are A Toy!")
     );
 
     return quotes.get(new Random().nextInt(quotes.size()));
